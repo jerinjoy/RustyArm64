91178bb4c773b9faf39c919a202aceb5f49d27b6
Jerin Joy <github@jerin.me>
2026-07-22 00:02:49 -0700

    wip - adding mmio support
    Signed-off-by: Jerin Joy <github@jerin.me>

--- simulator/src/bus.rs
+++ simulator/src/bus.rs
@@ -1,1 +1,27 @@
+use std::ops::RangeInclusive;
 
+use crate::io_device::IoDevice;
+
+#[derive(Default)]
+pub struct Bus {
+    devices: Vec<(RangeInclusive<u64>, Box<dyn IoDevice>)>,
+}
+
+impl Bus {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn register(&mut self, range: RangeInclusive<u64>, device: Box<dyn IoDevice>) {
+        self.devices.push((range, device));
+    }
+
+    fn find_device_mut(&mut self, address: u64) -> Option<&mut dyn IoDevice> {
+        for (range, device) in &mut self.devices {
+            if range.contains(&address) {
+                return Some(device.as_mut());
+            }
+        }
+        None
+    }
+}
